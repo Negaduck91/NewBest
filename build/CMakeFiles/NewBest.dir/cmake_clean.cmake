d40b0e1bf3463f134253af43d5c8d210a4a13055
Negaduck91 <apexadress@gmail.com>
2023-01-13 19:04:06 +0100

    added cmake

---
+++ build/CMakeFiles/NewBest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewBest.dir/main.cpp.o"
+  "CMakeFiles/NewBest.dir/main.cpp.o.d"
+  "NewBest"
+  "NewBest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewBest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
